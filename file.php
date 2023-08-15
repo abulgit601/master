98a262926f96df85a91ff76bb219723ca91a8321
Abul Middya [Sonata] <abul.middya-v@abfrl.adityabirla.com>
2023-08-15 22:47:56 +0530

    2nd commit

--- file.php
+++ file.php
@@ -1,5 +1,13 @@
 <?php
+class Mycontroller extends MY_Controller{
+	
+	function Mycontroller(){
+		parent::MY_Controller();
+		$this->load->model ( "ajio/testdropshipmodel" );
+	}
+    public function index(){
+        echo "test";die;
 
-echo "hello";die;
+    }
 
 ?>
